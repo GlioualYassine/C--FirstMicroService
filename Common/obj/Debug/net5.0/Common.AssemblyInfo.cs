e596481e8735c332c41a41fdf9566b56fa0e4ad1
GLIOUAL YASSINE <121353881+GlioualYassine@users.noreply.github.com>
2024-02-13 11:04:46 +0100

    safety commit

--- Common/obj/Debug/net5.0/Common.AssemblyInfo.cs
+++ Common/obj/Debug/net5.0/Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd3dd1bad4c259cbb17de250db375684b5e10d82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfef9dc0f4c5a2b20453bc0070e829bca73fd554")]
 [assembly: System.Reflection.AssemblyProductAttribute("Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Microservice 2 Inventory/Startup.cs
+++ Microservice 2 Inventory/Startup.cs
@@ -10,9 +10,12 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Polly;
+using Polly.Timeout;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Microservice_2_Inventory
@@ -28,13 +31,20 @@ namespace Microservice_2_Inventory
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
+        { 
             services.AddMongo()
                 .AddMongoRepository<InventoryItem>("inventoryitems");
+            Random iterer = new Random();
             services.AddHttpClient<CatalogClient>(client =>
             {
                 client.BaseAddress = new Uri("https://localhost:5001");    
-            });
+            })
+             .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(5, retryAttempt =>
+                 TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)) + TimeSpan.FromMilliseconds(iterer.Next(0,1000))
+             ))
+             .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().CircuitBreakerAsync(
+                 3,TimeSpan.FromSeconds(15)
+             )).AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1))  ;
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
